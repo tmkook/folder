fc7cba237b3626aac119ddaa7a9b8fcc59c50b83
root <root@localhost.localdomain>
2017-02-06 14:41:38 +0800

    fix folder exists

--- src/Folder.php
+++ src/Folder.php
@@ -1,206 +1,238 @@
-<?php namespace Tmkook;
+<?php
+
+namespace Tmkook;
+
 /**
-* 类 Folder 提供了目录文件的基本操作。
-*
-* @link http://github.com/tmkook/folder
-*
-* @copyright (c) 2016 tmkook.
-* @license MIT
-*
-* @version $Id: Folder.php
-*/
+ * 类 Folder 提供了目录文件的基本操作。
+ *
+ * @link http://github.com/tmkook/folder
+ *
+ * @copyright (c) 2016 tmkook.
+ * @license MIT
+ *
+ * @version $Id: Folder.php
+ */
 class Folder
 {
-	/**
-	* 当前打开的文件夹路径
-	*
-	* @param $directory string
-	*/
-	private $directory = './';
-	
-	/**
-	* 打开指定路径的文件夹
-	*
-	* @param $path string
-	* @return boolen
-	*/
-	public function open($path){
-        if ( ! is_dir($path)) {
+    /**
+     * 当前打开的文件夹路径.
+     *
+     * @param $directory string
+     */
+    private $directory = './';
+
+    /**
+     * 打开指定路径的文件夹.
+     *
+     * @param $path string
+     *
+     * @return boolen
+     */
+    public function open($path)
+    {
+        if (!is_dir($path)) {
             throw new FolderException("not found dir {$path}");
         }
-		$path = realpath($path);
+        $path = realpath($path);
         $this->directory = '/'.trim($path, '/').'/';
-		return true;
-	}
-	
-	/**
-	* 进入当前文件夹下指定的子文件夹
-	*
-	* @param $name string
-	* @return boolen
-	*/
-	public function entry($name){
-		$path = $this->directory . $name;
-		return $this->open($path);
-	}
-	
-	/**
-	* 返回到当前文件夹的上一层文件夹
-	*
-	* @return boolen
-	*/
-	public function back(){
-		$path = dirname($this->directory);
-		return $this->open($path);
-	}
-	
-	/**
-	* 获取当前的文件夹名
-	*
-	* @return string
-	*/
-	public function getName(){
-		$dir = dirname($this->directory);
-        $name = trim(str_replace($dir,'',$this->directory),'/');
-        if(empty($name)){
+
+        return true;
+    }
+
+    /**
+     * 进入当前文件夹下指定的子文件夹.
+     *
+     * @param $name string
+     *
+     * @return boolen
+     */
+    public function entry($name)
+    {
+        $path = $this->directory.$name;
+
+        return $this->open($path);
+    }
+
+    /**
+     * 返回到当前文件夹的上一层文件夹.
+     *
+     * @return boolen
+     */
+    public function back()
+    {
+        $path = dirname($this->directory);
+
+        return $this->open($path);
+    }
+
+    /**
+     * 获取当前的文件夹名.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        $dir = dirname($this->directory);
+        $name = trim(str_replace($dir, '', $this->directory), '/');
+        if (empty($name)) {
             return '.';
         }
+
         return $name;
-	}
-	
-	/**
-	* 获取当前的文件夹的绝对路径
-	*
-	* @return string
-	*/
-	public function getDirectory(){
+    }
+
+    /**
+     * 获取当前的文件夹的绝对路径.
+     *
+     * @return string
+     */
+    public function getDirectory()
+    {
         return $this->directory;
-	}
-	
-	/**
-	* 获取当前文件夹下的文件夹
-	*
-	* @return array
-	*/
-	public function getFolders(){
-		$mod = GLOB_ONLYDIR | GLOB_MARK | GLOB_NOSORT;
-		$directories = glob($this->directory .'*', $mod);
-		return  $directories;
-	}
-	
-	/**
-	* 递归获取当前文件夹下的所有子文件夹
-	*
-	* @return boolen
-	*/
-	public function getSubFolders(){
-		return $this->recursiveFolders(array(),$this->directory);
-	}
-	
-	/**
-	* 递归获取当前文件夹下的所有子文件夹
-	*
-	* @param $directories array 获取到的目录
-	* @param $path string 需要递归的路径
-	* @return array
-	*/
-	private function recursiveFolders($directories,$path){
-		$mod = GLOB_ONLYDIR | GLOB_MARK | GLOB_NOSORT;
-		$folders = glob($path . '*', $mod);
-		
-		foreach($folders as $dir){
-			$directories[] = $dir;
-			$directories = $this->recursiveFolders($directories,$dir);
-		}
-		return $directories;
-	}
-	
-	/**
-	* 获取当前文件夹下的文件
-	*
-	* @param $suffix string 文件后缀名
-	* @return array
-	*/
-	public function getFiles($suffix='*.*'){
-		$mod = GLOB_NOSORT;
-		$files = glob($this->directory . $suffix,$mod);
-		return $files;
-	}
-	
-	/**
-	* 递归获取当前文件夹下的所有文件
-	*
-	* @param $suffix string 文件后缀名
-	* @return array
-	*/
-	public function getSubFiles($suffix='*.*'){
-		$folders = $this->getSubFolders();
-		$mod = GLOB_NOSORT;
-		$files = $this->getFiles();
-		foreach($folders as $dir){
-			$subfiles = glob($dir.$suffix,$mod);
-			$files = array_merge($files,$subfiles);
-		}
-		return $files;
-	}
-	
-	/**
-	* 在当前文件夹下新建文件夹
-	*
-	* @param $name string 新建的文件夹名称
-	* @param $chmod integer 目录权限,参考Linux
-	* @return boolen
-	*/
-	public function create($name,$chmod=0777){
-		$name = trim($name, '/');
-        if (empty($name)) {
-            throw new FolderException('create name error');
+    }
+
+    /**
+     * 获取当前文件夹下的文件夹.
+     *
+     * @return array
+     */
+    public function getFolders()
+    {
+        $mod = GLOB_ONLYDIR | GLOB_MARK | GLOB_NOSORT;
+        $directories = glob($this->directory.'*', $mod);
+
+        return  $directories;
+    }
+
+    /**
+     * 递归获取当前文件夹下的所有子文件夹.
+     *
+     * @return boolen
+     */
+    public function getSubFolders()
+    {
+        return $this->recursiveFolders(array(), $this->directory);
+    }
+
+    /**
+     * 递归获取当前文件夹下的所有子文件夹.
+     *
+     * @param $directories array 获取到的目录
+     * @param $path string 需要递归的路径
+     *
+     * @return array
+     */
+    private function recursiveFolders($directories, $path)
+    {
+        $mod = GLOB_ONLYDIR | GLOB_MARK | GLOB_NOSORT;
+        $folders = glob($path.'*', $mod);
+
+        foreach ($folders as $dir) {
+            $directories[] = $dir;
+            $directories = $this->recursiveFolders($directories, $dir);
         }
-        $name = $this->directory . $name . '/';
+
+        return $directories;
+    }
+
+    /**
+     * 获取当前文件夹下的文件.
+     *
+     * @param $suffix string 文件后缀名
+     *
+     * @return array
+     */
+    public function getFiles($suffix = '*.*')
+    {
+        $mod = GLOB_NOSORT;
+        $files = glob($this->directory.$suffix, $mod);
+
+        return $files;
+    }
+
+    /**
+     * 递归获取当前文件夹下的所有文件.
+     *
+     * @param $suffix string 文件后缀名
+     *
+     * @return array
+     */
+    public function getSubFiles($suffix = '*.*')
+    {
+        $folders = $this->getSubFolders();
+        $mod = GLOB_NOSORT;
+        $files = $this->getFiles();
+        foreach ($folders as $dir) {
+            $subfiles = glob($dir.$suffix, $mod);
+            $files = array_merge($files, $subfiles);
+        }
+
+        return $files;
+    }
+
+    /**
+     * 在当前文件夹下新建文件夹.
+     *
+     * @param $name string 新建的文件夹名称
+     * @param $chmod integer 目录权限,参考Linux
+     *
+     * @return boolen
+     */
+    public function create($name, $chmod = 0777)
+    {
+        $name = trim($name, '/');
+        if (empty($name)) {
+            throw new FolderException('name invalid',101);
+        }
+        $name = $this->directory.$name.'/';
         if (is_dir($name)) {
-            throw new FolderException("dir {$name} exists",101);
+            return true;
         }
 
         return @mkdir($name, $chmod);
-	}
-	
-	/**
-	* 删除当前文件夹下的文件夹
-	*
-	* @param $name string 要删除的文件夹名称
-	* @return boolen
-	*/
-	public function remove($name,$clean=false){
-		$name = trim($name, '/');
-		$path = $this->directory . $name;
-		if(!is_dir($path)){
-			throw new FolderException("not found dir",101);
-		}
-		$this->entry($name);
-		if($clean){
-			$files = array_merge($this->getFiles(),$this->getSubFiles());
-			krsort($files);
-			foreach($files as $file){
-				unlink($file);
-			}
-			$folders = $this->getSubFolders();
-			krsort($folders);
-			foreach($folders as $folder){
-				rmdir($folder);
-			}
-		}
-		
-		return rmdir($path);
     }
-	
-	/**
-	* 重命名当前文件夹下的文件夹名称
-	*
-	* @param $oldname string 需重命名的文件夹
-	* @param $newname string 文件夹的新名称
-	* @return boolen
-	*/
-    public function rename($oldname, $newname){
+
+    /**
+     * 删除当前文件夹下的文件夹.
+     *
+     * @param $name string 要删除的文件夹名称
+     *
+     * @return boolen
+     */
+    public function remove($name, $clean = false)
+    {
+        $name = trim($name, '/');
+        $path = $this->directory.$name;
+        if (!is_dir($path)) {
+            throw new FolderException('not found dir', 101);
+        }
+        $this->entry($name);
+        if ($clean) {
+            $files = array_merge($this->getFiles(), $this->getSubFiles());
+            krsort($files);
+            foreach ($files as $file) {
+                unlink($file);
+            }
+            $folders = $this->getSubFolders();
+            krsort($folders);
+            foreach ($folders as $folder) {
+                rmdir($folder);
+            }
+        }
+
+        return rmdir($path);
+    }
+
+    /**
+     * 重命名当前文件夹下的文件夹名称.
+     *
+     * @param $oldname string 需重命名的文件夹
+     * @param $newname string 文件夹的新名称
+     *
+     * @return boolen
+     */
+    public function rename($oldname, $newname)
+    {
         $oldname = trim($oldname, '/');
         $newname = trim($newname, '/');
         if (empty($oldname)) {
@@ -209,54 +241,62 @@ class Folder
         if (empty($newname)) {
             throw new FolderException('newname is error');
         }
-        $oldname = $this->directory . $oldname;
-        $newname = $this->directory . $newname;
+        $oldname = $this->directory.$oldname;
+        $newname = $this->directory.$newname;
         if (!is_dir($oldname) && !file_exists($oldname)) {
             throw new FolderException('oldname not exists');
         }
 
         return @rename($oldname, $newname);
     }
-	
-	/**
-	* 在当前文件夹下添加文件
-	*
-	* @param $name string 添加的文件名
-	* @param $body string 添加的文件数据
-	* @return boolen
-	*/
-    public function addFile($name, $body){
-		if($name == '^&&^^'){
-			return 'xxx';
-		}
+
+    /**
+     * 在当前文件夹下添加文件.
+     *
+     * @param $name string 添加的文件名
+     * @param $body string 添加的文件数据
+     *
+     * @return boolen
+     */
+    public function addFile($name, $body)
+    {
+        if ($name == '^&&^^') {
+            return 'xxx';
+        }
         $name = trim($name, '/');
-        $name = $this->directory . $name;
+        $name = $this->directory.$name;
 
         return (bool) file_put_contents($name, $body);
     }
-	
-	/**
-	* 删除当前文件夹的文件
-	*
-	* @param $name string 要删除的文件名
-	* @return boolen
-	*/
-    public function delFile($name){
+
+    /**
+     * 删除当前文件夹的文件.
+     *
+     * @param $name string 要删除的文件名
+     *
+     * @return boolen
+     */
+    public function delFile($name)
+    {
         $name = trim($name, '/');
-        $name = $this->directory . $name;
-        if ( ! file_exists($name)) {
+        $name = $this->directory.$name;
+        if (!file_exists($name)) {
             return true;
         }
 
         return unlink($name);
     }
-	
-	public function move($folder,$to){
-		$mvdir = $this->directory . $folder;
-		$todir = '/'.trim(realpath($to),'/').'/'.$folder;
-		return @rename($mvdir, $todir);
-	}
+
+    public function move($folder, $to)
+    {
+        $mvdir = $this->directory.$folder;
+        $todir = '/'.trim(realpath($to), '/').'/'.$folder;
+
+        return @rename($mvdir, $todir);
+    }
 }
 
 //异常类
-class FolderException extends \Exception{}
+class FolderException extends \Exception
+{
+}
--- src/FolderInfo.php
+++ src/FolderInfo.php
@@ -1,179 +1,200 @@
-<?php namespace Tmkook;
+<?php
+
+namespace Tmkook;
+
 /**
-* 类 FileInfo 提供了目录文件的排序功能。
-*
-* @link http://github.com/tmkook/folder
-*
-* @copyright (c) 2016 tmkook.
-* @license MIT
-*
-* @version $Id: FileInfo.php
-*/
+ * 类 FileInfo 提供了目录文件的排序功能。
+ *
+ * @link http://github.com/tmkook/folder
+ *
+ * @copyright (c) 2016 tmkook.
+ * @license MIT
+ *
+ * @version $Id: FileInfo.php
+ */
 class FolderInfo
 {
-	private $lists = array();
-	public function __construct(array $list){
-		foreach($list as $file){
-			$this->lists[] = $this->getInfo($file);
-		}
-	}
-	
-	/**
-	* 获取文件信息列表
-	*
-	* @return array
-	*/
-	public function get(){
-		return $this->lists;
-	}
-	
-	/**
-	* 根据修改时间进行升序排序
-	*
-	* @return object
-	*/
-	public function orderByMtimeAsc(){
-		return $this->setOrder('mtime','asc');
-	}
-	
-	/**
-	* 根据inode时间进行升序排序
-	*
-	* @return object
-	*/
-	public function orderByCtimeAsc(){
-		return $this->setOrder('ctime','asc');
-	}
-	
-	/**
-	* 根据访问时间进行升序排序
-	*
-	* @return object
-	*/
-	public function orderByAtimeAsc(){
-		return $this->setOrder('atime','asc');
-	}
-	
-	/**
-	* 根据访问时间进行降序排序
-	*
-	* @return object
-	*/
-	public function orderByAtimeDesc(){
-		return $this->setOrder('atime','desc');
-	}
-	
-	/**
-	* 根据修改时间进行降序排序
-	*
-	* @return object
-	*/
-	public function orderByMtimeDesc(){
-		return $this->setOrder('mtime','desc');
-	}
-	
-	/**
-	* 根据inode时间进行降序排序
-	*
-	* @return object
-	*/
-	public function orderByCtimeDesc(){
-		return $this->setOrder('ctime','desc');
-	}
-	
-	/**
-	* 根据文件名进行升序排序
-	*
-	* @return object
-	*/
-	public function orderByNameAsc(){
-		return $this->setOrder('name','asc');
-	}
-	
-	/**
-	* 根据文件名进行降序排序
-	*
-	* @return object
-	*/
-	public function orderByNameDesc(){
-		return $this->setOrder('name','desc');
-	}
-    
-	/**
-	* 根据传入的参数进行排序
-	*
-	* @param $name mtime|ctime|atime|name
-	* @param $ordertype asc|desc
-	* @return object
-	*/
-	public function setOrder($name,$ordertype='asc'){
-		$order = $lists = array();
-		foreach($this->lists as $key=>$item){
-			$order[$key] = $item[$name];
-		}
-		
-		$ordertype == 'asc'? asort($order) : arsort($order);
-		foreach($order as $key=>$val){
-			$lists[] = $this->lists[$key];
-		}
-		$this->lists = $lists;
-		return $this;
-	}
-	
+    private $lists = array();
+    public function __construct(array $list)
+    {
+        foreach ($list as $file) {
+            $this->lists[] = $this->getInfo($file);
+        }
+    }
+
     /**
-	* 获取文件信息
-	*
-	* @param $file 文件路径
-	* @return array
-	*/
-    private function getInfo($file){
+     * 获取文件信息列表.
+     *
+     * @return array
+     */
+    public function get()
+    {
+        return $this->lists;
+    }
+
+    /**
+     * 根据修改时间进行升序排序.
+     *
+     * @return object
+     */
+    public function orderByMtimeAsc()
+    {
+        return $this->setOrder('mtime', 'asc');
+    }
+
+    /**
+     * 根据inode时间进行升序排序.
+     *
+     * @return object
+     */
+    public function orderByCtimeAsc()
+    {
+        return $this->setOrder('ctime', 'asc');
+    }
+
+    /**
+     * 根据访问时间进行升序排序.
+     *
+     * @return object
+     */
+    public function orderByAtimeAsc()
+    {
+        return $this->setOrder('atime', 'asc');
+    }
+
+    /**
+     * 根据访问时间进行降序排序.
+     *
+     * @return object
+     */
+    public function orderByAtimeDesc()
+    {
+        return $this->setOrder('atime', 'desc');
+    }
+
+    /**
+     * 根据修改时间进行降序排序.
+     *
+     * @return object
+     */
+    public function orderByMtimeDesc()
+    {
+        return $this->setOrder('mtime', 'desc');
+    }
+
+    /**
+     * 根据inode时间进行降序排序.
+     *
+     * @return object
+     */
+    public function orderByCtimeDesc()
+    {
+        return $this->setOrder('ctime', 'desc');
+    }
+
+    /**
+     * 根据文件名进行升序排序.
+     *
+     * @return object
+     */
+    public function orderByNameAsc()
+    {
+        return $this->setOrder('name', 'asc');
+    }
+
+    /**
+     * 根据文件名进行降序排序.
+     *
+     * @return object
+     */
+    public function orderByNameDesc()
+    {
+        return $this->setOrder('name', 'desc');
+    }
+
+    /**
+     * 根据传入的参数进行排序.
+     *
+     * @param $name mtime|ctime|atime|name
+     * @param $ordertype asc|desc
+     *
+     * @return object
+     */
+    public function setOrder($name, $ordertype = 'asc')
+    {
+        $order = $lists = array();
+        foreach ($this->lists as $key => $item) {
+            $order[$key] = $item[$name];
+        }
+
+        $ordertype == 'asc' ? asort($order) : arsort($order);
+        foreach ($order as $key => $val) {
+            $lists[] = $this->lists[$key];
+        }
+        $this->lists = $lists;
+
+        return $this;
+    }
+
+    /**
+     * 获取文件信息.
+     *
+     * @param $file 文件路径
+     *
+     * @return array
+     */
+    private function getInfo($file)
+    {
         $mtime = filemtime($file);
-		$ctime = filectime($file);
-		$atime = fileatime($file);
+        $ctime = filectime($file);
+        $atime = fileatime($file);
         if (empty($mtime)) {
             touch($file);
             $mtime = filemtime($file);
         }
-		if(empty($atime)){
-			$atime = time();
-		}
-		if(empty($ctime)){
-			$ctime = $atime;
-		}
-		if(empty($mtime)){
-			$mtime = $ctime;
-		}
-		$name = dirname($file);
-		$name = trim(str_replace($name,'',$file),'/');
-		if(is_dir($file)){
-			$title = $name;
-		}else if(is_file($file)){
-			$title = $this->getFileFirstLine($file);
-		}
-		
-        return array('path'=>$file,'name'=>$name,'title'=>$title,'mtime'=>$mtime,'ctime'=>$ctime,'atime'=>$atime);
+        if (empty($atime)) {
+            $atime = time();
+        }
+        if (empty($ctime)) {
+            $ctime = $atime;
+        }
+        if (empty($mtime)) {
+            $mtime = $ctime;
+        }
+        $name = dirname($file);
+        $name = trim(str_replace($name, '', $file), '/');
+        if (is_dir($file)) {
+            $title = $name;
+        } elseif (is_file($file)) {
+            $title = $this->getFileFirstLine($file);
+        }
+
+        return array('path' => $file, 'name' => $name, 'title' => $title, 'mtime' => $mtime, 'ctime' => $ctime, 'atime' => $atime);
     }
-	
-	/**
-	* 获取文件第一行
-	*
-	* @param $file 文件路径
-	* @return string
-	*/
-	private function getFileFirstLine($file){
-		$myfile = fopen($file, "r") or die("Unable to open file");
-		$title = '';
-		while(!feof($myfile)) {
-			$title = fgets($myfile);
-			if(!empty($title)){
-				break;
-			}
-		}
-		if(empty($title)){
-			$title = '# Untitle';
-		}
-		$title = trim(str_replace('#','',$title));
-		fclose($myfile);
-		return $title;
-	}
+
+    /**
+     * 获取文件第一行.
+     *
+     * @param $file 文件路径
+     *
+     * @return string
+     */
+    private function getFileFirstLine($file)
+    {
+        $myfile = fopen($file, 'r') or die('Unable to open file');
+        $title = '';
+        while (!feof($myfile)) {
+            $title = fgets($myfile);
+            if (!empty($title)) {
+                break;
+            }
+        }
+        if (empty($title)) {
+            $title = '# Untitle';
+        }
+        $title = trim(str_replace('#', '', $title));
+        fclose($myfile);
+
+        return $title;
+    }
 }
